6c73445a7f46e0b9d043392a534a118beaa8e341
Egor Tkachenko <egortka@egors-imac.local>
2019-05-31 01:55:03 +0700

    Initial Commit

---
+++ 1n2a1r/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  1n2a1r
+//
+//  Created by Egor Tkachenko on 31/05/2019.
+//  Copyright © 2019 ET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
