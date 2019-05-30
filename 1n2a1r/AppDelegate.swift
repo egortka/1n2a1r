6cea82c4978d90f0ce8f12920be751513fd227ba
Egor Tkachenko <egortka@egors-imac.local>
2019-05-31 02:14:57 +0700

    Initial Commit

--- 1n2a1r/AppDelegate.swift
+++ 1n2a1r/AppDelegate.swift
@@ -12,10 +12,13 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow()
+        window?.rootViewController = MainViewController()
+        
         return true
     }
 
